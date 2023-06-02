f822494b75a5b8a7cd0f452b086857fe32100773
abhay-111 <abhaychauhan11211@gmail.com>
2023-06-02 16:14:46 +0530

    feat : added side naviagtion bar

---
+++ components/ui/NavListItems.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const NavListItems = ({text,iconUrl,isActive}) => {
+  return (
+    <li className="flex items-center gap-[20px] w-full">
+      <img src={iconUrl} className="h-[18px] w-[18px]" alt="" />
+      <p
+        className={
+          "text-white text-[18px] leading-[22px] " +
+          (isActive ? "font-bold" : "font-[300]")
+        }
+      >
+        {text}
+      </p>
+    </li>
+  );
+}
